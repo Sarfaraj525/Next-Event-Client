75810575e7a54ccb7dd9e16e86827f54b93f3050
Sarfaraj Nawaz Chowdhury <sarfarajeee525@gmail.com>
2025-12-07 23:16:29 +0600

    Navbar created

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -20,16 +20,16 @@ const navLinks = [
 
 export default function Navbar() {
   const { user, isLoading } = useUser();
-  // console.log(user);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full shadow-sm backdrop-blur-sm bg-black/20">
+    <header className="fixed top-0 left-0 z-50 w-full shadow-sm backdrop-blur-md bg-slate-900/30">
       <div className="container flex items-center justify-between h-20 px-4 mx-auto md:px-8">
+
         {/* Logo */}
         <Link href="/" className="flex items-center">
           <Image
-            src='/favicon.png'
+            src="/favicon.png"
             alt="Logo"
             width={1000}
             height={1000}
@@ -41,14 +41,17 @@ export default function Navbar() {
         <ul className="hidden gap-8 text-sm font-medium md:flex">
           {navLinks.map((link) => (
             <li key={link.href}>
-              <Link href={link.href} className="navButton !bg-slate-50/10">
+              <Link
+                href={link.href}
+                className="navButton !bg-white/10 hover:!bg-blue-500/20"
+              >
                 {link.label}
               </Link>
             </li>
           ))}
         </ul>
 
-        {/* User Actions & Mobile Toggle */}
+        {/* User Actions */}
         <nav className="flex items-center gap-2">
           {!isLoading &&
             (user ? (
@@ -59,7 +62,7 @@ export default function Navbar() {
                   </Link>
                 ) : (
                   <Link href="/profile/personal-info" className="relative">
-                    <Avatar className="cursor-pointer ring-2 ring-blue-500 hover:ring-4 transition duration-300">
+                    <Avatar className="cursor-pointer ring-2 ring-blue-400 hover:ring-4 transition duration-300">
                       <AvatarImage src={user.profileImage} alt="Profile" />
                       <AvatarFallback>
                         {user.name?.[0]?.toUpperCase() || "U"}
@@ -79,7 +82,7 @@ export default function Navbar() {
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
             className="inline-flex items-center justify-center p-2 ml-2 md:hidden"
           >
-            <Menu className="w-6 h-6 text-white" />
+            <Menu className="w-6 h-6 text-blue-400" />
           </button>
         </nav>
       </div>
